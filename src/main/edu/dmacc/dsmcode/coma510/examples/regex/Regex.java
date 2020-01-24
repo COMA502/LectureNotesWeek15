054e45b8e165933005a49c4d6fa2c5468587a5df
Gregory Hazen <gmhazen@dmacc.edu>
2020-01-24 14:31:21 -0600

    Added an example of regex

--- src/main/edu/dmacc/dsmcode/coma510/examples/regex/Regex.java
+++ src/main/edu/dmacc/dsmcode/coma510/examples/regex/Regex.java
@@ -2,6 +2,12 @@ package edu.dmacc.dsmcode.coma510.examples.regex;
 
 public class Regex {
     public static void main(String[] args) {
-        //todo: regular expressions exercise
+        String taxIdPattern = "\\d{2,3}-\\d{2}-\\d{4}</input><script>do something</script>";
+        String gregsTaxId = " 111-11-1111";
+        if(!gregsTaxId.trim().matches(taxIdPattern)) {
+            System.out.println("Error, not a valid tax ID... :'(");
+        }
+
+        String emailPattern = "";
     }
 }
